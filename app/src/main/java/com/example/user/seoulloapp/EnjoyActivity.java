c8ea99d5d6402dfd121afbebcd2ead6bdd3440ea
chaehyeon <eprtm22@naver.com>
2017-09-28 19:22:41 +0900

    9.28 7:22pm

---
+++ app/src/main/java/com/example/user/seoulloapp/EnjoyActivity.java
@@ -0,0 +1,14 @@
+package com.example.user.seoulloapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+//서울로즐기기 지도
+public class EnjoyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enjoy);
+    }
+}
